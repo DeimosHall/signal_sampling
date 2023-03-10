2abfadea1a3c5cb39aa31365b774ebfa7851b8f5
Deimos <deimoshall@gmail.com>
2023-03-10 14:50:25 -0600

    fix: return to the sampling python script

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 
 import serial
 import matplotlib.pyplot as plt
-import time
 
 port = '/dev/ttyUSB0'
 baudrate = 115200
@@ -14,33 +13,20 @@ fig, ax = plt.subplots()
 xs = []
 ys = []
 
-# Set a flag to stop the loop after 50 milliseconds
-stop_flag = False
-start_time = time.time()
-stop_time = start_time + 0.05  # 50 milliseconds
-
-while not stop_flag:
+while True:
     # Read a line of data from the serial port
     data = ser.readline().decode().rstrip()
-
+    
     # Convert the line of data to a number
     try:
         y = float(data)
     except ValueError:
         continue
-
+    
     xs.append(len(xs))
     ys.append(y)
-
+    
     ax.clear()
     ax.plot(xs, ys)
     plt.draw()
     plt.pause(0.001)
-
-    # Check if the stop time has been reached
-    if time.time() >= stop_time:
-        stop_flag = True
-
-# Close the serial connection and plot
-ser.close()
-plt.show()
