d7ff5746fbf2324050eafd934882f47570cda44f
Deimos <deimoshall@gmail.com>
2023-03-10 10:18:28 -0600

    feat: this is the good

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 import serial
 import matplotlib.pyplot as plt
+import time
 
 port = '/dev/ttyUSB0'
 baudrate = 115200
@@ -13,20 +14,33 @@ fig, ax = plt.subplots()
 xs = []
 ys = []
 
-while True:
+# Set a flag to stop the loop after 50 milliseconds
+stop_flag = False
+start_time = time.time()
+stop_time = start_time + 0.05  # 50 milliseconds
+
+while not stop_flag:
     # Read a line of data from the serial port
     data = ser.readline().decode().rstrip()
-    
+
     # Convert the line of data to a number
     try:
         y = float(data)
     except ValueError:
         continue
-    
+
     xs.append(len(xs))
     ys.append(y)
-    
+
     ax.clear()
     ax.plot(xs, ys)
     plt.draw()
     plt.pause(0.001)
+
+    # Check if the stop time has been reached
+    if time.time() >= stop_time:
+        stop_flag = True
+
+# Close the serial connection and plot
+ser.close()
+plt.show()
--- signal_sampling.ino
+++ signal_sampling.ino
@@ -1,18 +1,38 @@
-#define SIGNAL 15
+#define SIGNAL 34
 
-int frequency = 20;
-unsigned long lastTime = 0, samplingTime = 1 / frequency;
+enum DelayTime {
+  t20Hz = 50000,
+  t50Hz = 20000,
+  t100Hz = 10000,
+  t1kHz =  1000,
+  t10kHz = 100,
+};
+
+unsigned long samplingTime = t10kHz;
+int bits[8] = {2, 4, 5, 18, 19, 21, 22, 23};
 
 void setup() {
-  pinMode(SIGNAL, INPUT);
   Serial.begin(115200);
+  pinMode(SIGNAL, INPUT);
+  for(int i = 0; i < 8; i++) {
+    pinMode(bits[i], OUTPUT);
+  }
+  // Serial.println("Sampling time: " + String(samplingTime));
+  // delay(3000);
 }
 
 void loop() {
-  if (millis() - lastTime >= samplingTime) {
-    lastTime = millis();
-    int adcValue = analogRead(SIGNAL);
-    float voltage = adcValue * 3.3 / 4095;
-    Serial.println(voltage);
+  int adcValue = analogRead(SIGNAL);
+
+  int bitsVal = map(adcValue, 0, 4095, 0, 255);
+  Serial.println(bitsVal);
+  for(int i = 0; i < 8; i++) {
+    if(bitsVal & (1 << i)) {
+      digitalWrite(bits[i], HIGH);
+    } else {
+      digitalWrite(bits[i], LOW);
+    }
   }
+
+  delayMicroseconds(samplingTime);
 }
